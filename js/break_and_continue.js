ab4e65e47b8c3eaf85e685400ae40787f282accc
Corey Koontz <corey.michael.koontz@gmail.com>
2021-01-03 01:06:35 -0600

    restarting excercise after code crashed computer

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -14,23 +14,23 @@ function validNum(input) {
     return (input >= 1 && input <= 50) && (input % 2 !== 0) && (!isNaN(input));
 }
 
-
-
-
-
-for (var i = 1; i <= 50; i++) {
-    var userNum = parseFloat(prompt("Please enter an odd number between 1 and 50"));
-    if ()//((userNum % 2 !== 0) && (userNum >= 1 && userNum <= 50) && (!isNaN(userNum))) {
-        alert("Yep, that's the right answer.");
-        while (i <= 50) {
-            console.log("Here is an odd number: " + i);
-            i++;
-        }
-    }else {
-        alert("Nope! Try again.")
-        continue;
-    }
-}
+// ****************  THIS CODE BELOW CRASHED THE COMPUTER DO NOT RUN! **********************************
+//for (var i = 1; i <= 50; i++) {
+//    var userNum = prompt("Please enter an odd number between 1 and 50");
+//    if (validNum(userNum)) { //if ((userNum % 2 !== 0) && (userNum >= 1 && userNum <= 50) && (!isNaN(userNum))) {
+//        alert("Yep, that's the right answer.");
+//        while (i <= 50) {
+//            if (validNum(i)) {
+//            console.log("Here is an odd number: " + i);
+//            } else {
+//                continue;
+//            }
+//        }i++;
+//    }else {
+//        alert("Nope! Try again.")
+//        continue;
+//    }
+//}
 
 //if (!(userNum <= 50 && userNum >=1)) {
 
