50b5393c6616e0cd859474f742fb2e375fe602f6
Corey Koontz <corey.michael.koontz@gmail.com>
2021-01-04 14:17:51 -0600

    feat: Complete planets-array exercise

--- js/iterating.js
+++ js/iterating.js
@@ -6,41 +6,51 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
+
 var names = ['Jim', 'Pam', 'Michael', 'Dwight']
+
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
+
 console.log(names.length);
+
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
+
 console.log(names[0]);
-    console.log(names[1]);
-    console.log(names[2]);
-    console.log(names[3]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
+
 for (var i = 0; i < names.length; i++) {
     console.log("The name at index " + i + " is: " + names[i]);
     }
+
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+
 names.forEach(function (name, index) {
     console.log("The name at index " + index + " is: " + name);
 });
+
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
-     * return an an element from it
+      * return an an element from it
      * - first: returns the first item in the array
      * - second: returns the second item in the array
      * - last: returns the last item in the array
@@ -50,17 +60,19 @@ names.forEach(function (name, index) {
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
-function firstElement (input) {
+
+function first (input) {
     return input[0];
 }
 
-function secondElement (input) {
+function second (input) {
     return input[1];
-    }
-function  lastElement (input) {
+}
+
+function  last (input) {
     return input[input.length -1]; // .length -(number) allows you to index from the other end of an array
 }
-console.log(firstElement(names));
-console.log(secondElement(names));
-console.log(lastElement(names));
+console.log(first(names));
+console.log(second(names));
+console.log(last(names));
 })();
