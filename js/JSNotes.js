e959e1f7c2f970df50f58b78ae28ffd029529e56
Corey Koontz <corey.michael.koontz@gmail.com>
2021-01-08 09:56:42 -0600

    adding a js notes page to the web exercises

---
+++ js/JSNotes.js
@@ -0,0 +1,45 @@
+// 1) Why use a for loop vs. a forEach and vice versa?
+
+// A for each loop is cleaner and can provide more readability.
+
+// 2) What is a higher order function?
+
+// A higher order function is a function that can take in other functions as arguments.
+
+// 3) What is a callback function?
+
+// A callback function is a function that is to be executed after another function has finished executing.
+// Also, any function that is passed as an argument is a callback function.
+
+// 4) What is the difference between =, ==, and ===?
+
+// = is assigning a value, while == (value) and === (value & type) are comparison operators
+
+// 5) What is an IIFE and why is best practice to wrap your JS code in one?
+
+// An IIFE is a container of sorts that isolates variable declarations from the global scope. They also
+// execute functions immediately.
+
+// 6) What is Strict Mode and why is a best practice to use it when writing JS code?
+
+// strict mode is called at the beginning of your js code to provide stricter parameters within the syntax.
+// It is best practice to use this to help catch errors.
+
+// 7) What kind of data is best stored in an array and what kind of data is best stored in an object?
+
+
+
+// 8) Why define an object in a single statement vs. dynamically adding properties and values in seperate statements?
+
+// Single statement should be used when you have all the data you need then and there.
+
+// 9) Are strings in JS mutable or immutable? What about arrays and objects?
+
+
+// Why does the following code log the number 3 and not the number 1?
+//   var x = {a: 1, b:2};
+//   var y = x;
+//   x.a = 3;
+//   console.log(y.a);
+
+// Because the values, even though they have different names, are still the same values.
