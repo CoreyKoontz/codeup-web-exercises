43ef83215bcad1d6cfa0a158701ad0c11865ded9
Corey Koontz <corey.michael.koontz@gmail.com>
2021-01-21 09:39:53 -0600

    Adding morning pushup exercise

--- js/morning-pushups.js
+++ js/morning-pushups.js
@@ -1,3 +1,13 @@
+//20 Jan
 function seven () {
     return 7;
+}
+
+//21 Jan
+function isMultipleOf (x, y) {
+    if (typeof x ==="number" && typeof y === "number") {
+    return x % y === 0;}
+    else {
+        return false;
+    }
 }
