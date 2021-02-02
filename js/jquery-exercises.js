e72c4cdd0042e50889011b27dff2f18f149920cb
Corey Koontz <corey.michael.koontz@gmail.com>
2021-02-02 14:36:18 -0600

    Complete jquery-selectors exercise

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,5 +1,15 @@
 "use strict"
 
-$(document).ready(function() {
-    alert ('Finished Loading')
+$(document).ready(function () {
+
+    var contents = $('#title').html();
+    alert(contents);
+
+    $('.codeup').css('border', '1px solid red');
+
+    $('li').css('font-size', '20px');
+    $('h1, p, li').css('background-color', 'lightgrey')
+
+    var h1elements = $('h1').html();
+    alert(h1elements);
 });
