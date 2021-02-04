bfe4032a313228f1f55be82a927a48e1663b6291
Corey Koontz <corey.michael.koontz@gmail.com>
2021-02-04 15:22:51 -0600

    Adding completed jquery_faq exercise. Still need bonus

--- js/scratchFile.js
+++ js/scratchFile.js
@@ -1,32 +1,76 @@
-"use strict"
+/**
+ * Several things to note in this file:
+ *
+ * - code organization: we define all our functions first, then attach them as event listeners
+ * - naming conventions:
+ *     - event listener function names start with "on" or "handle"
+ *     - variables that hold jQuery objects have a "$" in front of their name
+ */
 
-//  jQuery
-
-$(document).ready(function () {
-
-
-});
-
-//When title text is clicked it will change to a random color
-var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-$('#title').click(function () {
-    $(this).css('color', randomColor);
-})
-
-// double-click the list and it will get a border
-$('p').dblclick(function () {
-    $(this).css('fontSize', '18px')
-});
-
-//hover over footer and it will become highlighted
-$('.footer').hover(
-    function () {
-        $(this).css({
-            backgroundColor: '#FF0',
-            transition: '.5s'
-        })
-    },
-    function () {
-        $(this).css('background-color', '');
+// obviously this is a little simplified, but works for our purposes
+function getPasswordStrength(password) {
+    if (password.length < 5) {
+        return "weak";
+    } else if (password.length < 8) {
+        return "moderate";
+    } else {
+        return "strong";
     }
-);
+}
+
+function onPasswordInput(e) {
+    // A slightly better way to write this might be with .closest (https://api.jquery.com/closest/),
+    // i.e.
+    //     $formGroup = $(e.target).closest('.row')
+    // To find the closest parent with the class of "row".
+    // However, .closest isn't in the curriculum, so we'll show a solution with .parent here.
+    const $formGroup = $(e.target).parent().parent().parent();
+    const strength = getPasswordStrength(e.target.value);
+
+    // clear out any old classes
+    $formGroup.removeClass(["text-danger", "text-warning", "text-success"]);
+
+    // add the right class
+    if (strength === "weak") {
+        $formGroup.addClass("text-danger");
+    } else if (strength === "moderate") {
+        $formGroup.addClass("text-warning");
+    } else {
+        $formGroup.addClass("text-success");
+    }
+
+    $('#password-feedback').text(strength);
+}
+
+function onConfirmPasswordInput(e) {
+    $formGroup = $(e.target).parent().parent().parent();
+    const passwordConfirmation = e.target.value;
+    const password = $('#password').val();
+
+    $formGroup.removeClass(["text-success", "text-danger"])
+
+    if (password === passwordConfirmation) {
+        $formGroup.addClass("text-success");
+    } else {
+        $formGroup.addClass("text-danger");
+    }
+}
+
+function handleNextButtonClick(e) {
+    const $currentSection = $('.active');
+    const $nextSection = $('.active').next();
+    const $currentPage = $('#current-page');
+
+    // update section visibility
+    $currentSection.removeClass('active').addClass('d-none');
+    $nextSection.removeClass('d-none').addClass('active');
+
+    // update current page display
+    const nextPage = parseInt($currentPage.text()) + 1;
+    console.log(nextPage);
+    $currentPage.text(nextPage);
+}
+
+$('#password').on("input", onPasswordInput);
+$('#confirm-password').on("input", onConfirmPasswordInput);
+$('.next-button').click(handleNextButtonClick);
