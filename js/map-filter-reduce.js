1f0383de15cdf28f798c99a14f9b5b03b431bb65
Corey Koontz <corey.michael.koontz@gmail.com>
2021-02-16 18:21:26 -0600

    Adding progess on map-filter-reduce exercise

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -35,3 +35,44 @@ const users = [
         yearsOfExperience: 9
     }
 ];
+
+
+// .filter
+const leastThreeLang = users.filter(user =>
+    user.languages.length >= 3);
+console.log(leastThreeLang);
+//(3) [{…}, {…}, {…}]
+// 0: {id: 2, name: "luis", email: "luis@codeup.com", languages: Array(3), yearsOfExperience: 6}
+// 1: {id: 4, name: "fernando", email: "fernando@codeup.com", languages: Array(3), yearsOfExperience: 8}
+// 2: {id: 5, name: "justin", email: "justin@codeup.com", languages: Array(4), yearsOfExperience: 9}
+// length: 3
+// __proto__: Array(0)
+
+
+// .map
+const userEmails = users.map(user =>
+    user.email);
+console.log(userEmails);
+//  (5)["ryan@codeup.com", "luis@codeup.com", "zach@codeup.com",
+//  "fernando@codeup.com", "justin@codeup.com"]
+
+
+// .reduce
+const totalYearsOfExperience = users.reduce((total, user) => {
+    return total + user.yearsOfExperience;
+}, 0);
+console.log(totalYearsOfExperience);
+// 35
+
+
+// .reduce
+let longestEmail = userEmails.reduce((a, b) =>
+    a.length > b.length ? a : b);
+console.log(longestEmail);
+//fernando@codeup.com
+
+
+// .reduce
+let userNames = users.reduce((name, user) =>
+    users.name);
+console.log(userNames);
