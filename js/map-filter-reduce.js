e077a02dec8799bc6cf769c46abb96b00e1d2214
Corey Koontz <corey.michael.koontz@gmail.com>
2021-02-23 16:22:34 -0600

    map-filter-reduce ecercise complete

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -37,9 +37,12 @@ const users = [
 ];
 
 
-// .filter
-const leastThreeLang = users.filter( user =>
-    user.languages.length >= 3);
+// TODO .filter -  create an array of user objects where each user object has at least 3 languages in the languages array:
+
+let leastThreeLang = users.filter( user => {
+    return user.languages.length >= 3
+});
+
 console.log(leastThreeLang);
 //(3) [{…}, {…}, {…}]
 // 0: {id: 2, name: "luis", email: "luis@codeup.com", languages: Array(3), yearsOfExperience: 6}
@@ -48,43 +51,77 @@ console.log(leastThreeLang);
 // length: 3
 // __proto__: Array(0)
 
+//-----------------------------------------------------------------------------------------
 
-// .map
-const userEmails = users.map( user =>
-    user.email);
+// TODO .map - create an array of strings where each element is a user's email address:
+
+let userEmails = users.map( user => {
+    return user.email
+});
+
 console.log(userEmails);
 //  (5)["ryan@codeup.com", "luis@codeup.com", "zach@codeup.com",
 //  "fernando@codeup.com", "justin@codeup.com"]
 
+//-----------------------------------------------------------------------------------------
 
-// .reduce
-const totalYearsOfExperience = users.reduce((total, user) => {
+// TODO .reduce - get the total years of experience from the list of users.
+//  Once you get the total of years you can use the result to calculate the average:
+
+let totalYearsOfExperience = users.reduce((total, user) => {
     return total + user.yearsOfExperience;
 }, 0);
+
 console.log(totalYearsOfExperience);
 // 35
 
+//-----------------------------------------------------------------------------------------
 
-// .reduce
-let longestEmail = userEmails.reduce((a, b) =>
-    a.length > b.length ? a : b);
+// TODO .reduce - get the longest email from the list of users:
+
+let longestEmail = userEmails.reduce((a, b) => {
+   return a.length > b.length ? a : b
+});
+
 console.log(longestEmail);
 //fernando@codeup.com
 
+//-----------------------------------------------------------------------------------------
 
-// .reduce
-let userNames = users.reduce((newArr, user) => {
+// TODO .reduce - get the list of user's names in a single string.
+//  Example: Your instructors are: ryan, luis, zach, fernando, justin.
+
+// TODO: Put lines 91-100 into a single function
+let userNames = users.reduce(function (newArr, user) {
     newArr.push(user.name);
     return newArr;
 }, []);
-// Got it to return the array of names but not able to insert it into a string
+
+let userNameStr = userNames.join(', ');
+
+function instructorNames () {
+    return `Your instructors are: ${userNameStr}.`;
+}
+//Output: "Your instructors are: ryan, luis, zach, fernando, justin."
+
+
+//This returns a string but not in the right format:
+// let userNames = users.reduce(function (total, user) {
+//     return total + " " + user.name;
+// }, " ");
 
 
 
 // ========================= LECTURE NOTES =========================================
 //23 Feb 2021
 
-// -Both functions map() and filter() you are getting back a new array.
+// - Both functions map() and filter() are returning a new array.
 //      - So we have to save that return value somewhere. most likely a variable.
-//
+// - For map() you should treat the inside of the brackets like you are doing a for loop
+//   because you are iterating through each of the items in the array.
+// - for filter() you will need a boolean expression and if it is true the filter will return the set value
+
+// - reduce() can take up to 4 arguments: accumulator(must be first), currentValue(must be second), index(optional)(3rd), initialValue(optional)(4th).
+//      - accumulator is what is returned. kinda like the bucket idea that holds all the accumulated values
+//      - the initalValue will set a starting value. if it is not supplied the initalValue will start at the current item of the array.
 
